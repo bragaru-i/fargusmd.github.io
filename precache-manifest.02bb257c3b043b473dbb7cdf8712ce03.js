d629a6368720ab898db66d98a321d464fa90f395
fargusMD <bragaruion@gmail.com>
2020-11-09 21:51:25 +0300

    start

---
+++ precache-manifest.02bb257c3b043b473dbb7cdf8712ce03.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26aa529e90eae9d75fda1adebd6ef17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a81e2d9732b51cabb6b",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "56dec19e11c76c0ba778",
+    "url": "/static/css/main.61a90857.chunk.css"
+  },
+  {
+    "revision": "1a81e2d9732b51cabb6b",
+    "url": "/static/js/2.8355bfe9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8355bfe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56dec19e11c76c0ba778",
+    "url": "/static/js/main.f98cc047.chunk.js"
+  },
+  {
+    "revision": "f12a1f31d936642a56cc",
+    "url": "/static/js/runtime-main.b0b71f1d.js"
+  },
+  {
+    "revision": "54ae0457fa2049d45d5d11e2d28a2a2c",
+    "url": "/static/media/casino-api.54ae0457.png"
+  },
+  {
+    "revision": "b07fc9e3a3e0a25fa24949e38fd0908e",
+    "url": "/static/media/ecommerce.b07fc9e3.png"
+  },
+  {
+    "revision": "7f29fa4e44ac624528d418ff3c266bda",
+    "url": "/static/media/food-travel.7f29fa4e.png"
+  },
+  {
+    "revision": "8ffc6e2d1e0c928991e854e4ba0218d7",
+    "url": "/static/media/footer.8ffc6e2d.jpg"
+  },
+  {
+    "revision": "3f57f32ad4352d3d8a2b8386fac36906",
+    "url": "/static/media/hero2.3f57f32a.jpg"
+  },
+  {
+    "revision": "6baa81e9cfbbc338f44c5a9409bc90b3",
+    "url": "/static/media/ls-blog.6baa81e9.png"
+  },
+  {
+    "revision": "8bce280a3a9829a908cafadf3056ed8c",
+    "url": "/static/media/my-beautiful-awesome-face.8bce280a.jpg"
+  },
+  {
+    "revision": "64a3fce0a2b73feac420243233d207f6",
+    "url": "/static/media/s7.64a3fce0.png"
+  },
+  {
+    "revision": "2c52760f14cc8238ff74d67d96112693",
+    "url": "/static/media/travel-agency.2c52760f.jpg"
+  }
+]);
